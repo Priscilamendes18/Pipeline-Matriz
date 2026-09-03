a11cd9f2e8d1a4c6b123b1da0f5cf65ff2d77597
Priscila Mendes <priscila.mendes2@fatec.sp.gov.br>
2026-09-03 01:07:13 -0300

    Teste SonarQube

--- calculadora.py
+++ calculadora.py
@@ -14,3 +14,9 @@ def divisao(a, b):
     if b == 0:
         raise ValueError("Não é possível dividir por zero.")
     return a / b
+
+def calcular_total(valor):
+    resultado = valor * 2
+    numero = 10
+
+    return resultado
